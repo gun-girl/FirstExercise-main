00673e8a571037dd01838aa376c2dce053c324d1
Shadi <you@example.com>
2024-01-13 22:50:32 +0200

    Exercise done

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById("actionButton").addEventListener("click", function() {
     alert("Button clicked!");
+    paragraph = document.getElementById("description");
+    paragraph.style.visibility = "hidden";
 });
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+
+
+
